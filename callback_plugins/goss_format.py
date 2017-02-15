d1bfaee6a65caf4b5191fb849334c02dce6aa11e
Naftuli Kay <rfkrocktk@gmail.com>
2017-02-14 17:04:04 -0800

    Complete Travis CI Testing
    And pretty output too.

--- callback_plugins/goss_format.py
+++ callback_plugins/goss_format.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 
+from ansible import constants as C
 from ansible.plugins.callback import CallbackBase
 
 import json
@@ -17,20 +19,24 @@ class CallbackModule(CallbackBase):
     CALLBACK_NAME = 'goss_format'
 
     def print_goss_output(self, result):
-        ansible_result = result
-        result = ansible_result._result
+        result = result._result
+
+        # insert newline
+        print()
 
         for output in ('stdout', 'stderr',):
             value = result.get(output, '')
 
             if PROLLY_JSON.search(value):
                 try:
-                    print(json.dumps(json.loads(value), indent=2))
+                    self._display.display(json.dumps(json.loads(value), indent=2), color=C.COLOR_ERROR)
                 except:
-                    print(value)
+                    self._display.display(value, color=C.COLOR_ERROR)
             else:
-                print(value)
+                self._display.display(value, color=C.COLOR_ERROR)
 
-    def v2_runner_on_failed(self, result, ignore_errors=False):
+    def v2_runner_item_on_failed(self, result, ignore_errors=False):
+        # import pdb ; pdb.set_trace()
+
         if result._result.get('invocation', {}).get('module_name') == 'goss':
             self.print_goss_output(result)
