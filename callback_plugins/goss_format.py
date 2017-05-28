18fd37391406761833bb204d29167bbb78db598d
Naftuli Kay <me@naftuli.wtf>
2017-05-28 12:24:53 -0700

    Refactor
    Many changes:
    
     1. Fixed output display.
     2. Fixed multiple files.
     3. Fixed variable names.
     4. No more cron dependency.
     5. Working output.
     6. Cleans up.

--- callback_plugins/goss_format.py
+++ callback_plugins/goss_format.py
@@ -7,10 +7,6 @@ from ansible import constants as C
 from ansible.plugins.callback import CallbackBase
 
 import json
-import re
-
-
-PROLLY_JSON = re.compile(r'^[\{\[]', re.I)
 
 
 class CallbackModule(CallbackBase):
@@ -18,25 +14,47 @@ class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_NAME = 'goss_format'
 
-    def print_goss_output(self, result):
-        result = result._result
-
-        # insert newline
-        print()
-
-        for output in ('stdout', 'stderr',):
+    def print_goss_output(self, result, output_format):
+        for output in ('stdout', 'stderr'):
             value = result.get(output, '')
 
-            if PROLLY_JSON.search(value):
+            if len(value) == 0:
+                # idgaf
+                continue
+
+            if output_format == "json":
                 try:
-                    self._display.display(json.dumps(json.loads(value), indent=2), color=C.COLOR_ERROR)
+                    value = json.dumps(json.loads(value), indent=2)
                 except:
-                    self._display.display(value, color=C.COLOR_ERROR)
-            else:
-                self._display.display(value, color=C.COLOR_ERROR)
+                    pass
+
+            if output_format == "rspecish":
+                # offer a newline
+                value = "\n{}".format(value)
+
+            self._display.display(value, color=C.COLOR_ERROR)
+
+    def failure_funnel(self, result):
+        if not result.is_failed():
+            # do. not. care.
+            return
+
+        task = result._task
+        loader = task._loader if task else None
+        host = result._host
+        role = task._role
+        play = role._play if role else None
+        vm = task._variable_manager
+
+        if 'format_goss_output' in task.tags:
+            # we have caught us a marlin
+            facts = vm.get_vars(loader=loader, task=task, host=host, play=play) if vm else {}
+            self.print_goss_output(facts.get('goss_output', {}), facts.get('goss_output_format', 'rspecish'))
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        self.failure_funnel(result)
+        # debug_during_failure("goss_format.py: CallbackModule.v2_runner_on_failed", result)
 
     def v2_runner_item_on_failed(self, result, ignore_errors=False):
-        # import pdb ; pdb.set_trace()
-
-        if result._result.get('invocation', {}).get('module_name') == 'goss':
-            self.print_goss_output(result)
+        self.failure_funnel(result)
+        # debug_during_failure("goss_format.py: CallbackModule.v2_runner_item_on_failed", result)
--- library/goss.py
+++ library/goss.py
@@ -6,113 +6,81 @@ from ansible.module_utils.basic import *
 DOCUMENTATION = '''
 ---
 module: goss
-author: Mathieu Corbin
-short_description: Launch goss (https://github.com/aelsabbahy/goss) test
+author: Naftuli Kay
+short_description: Execute a Goss Test
 description:
-    - Launch goss test. Always changed = False if success.
+  - Executes a Goss test. Changed always false if tests succeeded
 options:
-    path:
-        required: true
-        description:
-            - Test file to validate. Must be on the remote machine.
-    format:
-        required: false
-        description:
-            - change the output goss format.
-            - Goss format list : goss v --format => [documentation json junit nagios rspecish tap].
-            - Default: rspecish
-    output_file:
-        required: false
-        description:
-            - save the result of the goss command in a file whose path is output_file
-examples:
-    - name: test goss file
-      goss:
-        path: "/path/to/file.yml"
+  path:
+    required: true
+    description:
+      - A Goss test file to execute, locally available on the remote machine.
+  format:
+    required: false
+    default: rspecish
+    choices: [rspecish, documentation, json, tap, junit, nagios, nagios_verbose, silent]
+    description:
+      - Modify the Goss output format.
+  executable:
+    required: false
+    default: goss
+    description:
+      - The executable to use when invoking Goss.
 
-    - name: test goss files
-      goss:
-        path: "{{ item }}"
-        format: json
-        output_file : /my/output/file-{{ item }}
-      with_items: "{{ goss_files }}"
+examples:
+  - name: execute goss file
+    goss: path=/path/to/file.yml
+
+  - name: execute goss file with json output
+    goss: path=/path/to/file.yml format=json
+
+  - name: custom goss
+    goss: path=/path/to/file.yml executable=/tmp/goss
 '''
 
+GOSS_OUTPUT_FORMATS = ("rspecish", "documentation", "json", "tap", "junit", "nagios", "nagios_verbose", "silent")
 
 # launch goss validate command on the file
-def check(module, test_file_path, output_format, executable='goss'):
+def evaluate(module, test_file, output_format, executable):
+    return module.run_command("{0} -g {1} validate --format {2}".format(executable, test_file, output_format))
 
-    output_format = 'rspecish' if not output_format else output_format
+def succeed(module, **kwargs):
+    module.exit_json(changed=False, failed=False, goss_failed=False, **kwargs)
 
-    return module.run_command("{0} -g {1} v --format {2}".format(executable, test_file_path, output_format))
-
-
-# write goss result to output_file_path
-def output_file(output_file_path, out):
-    if output_file_path is not None:
-        with open(output_file_path, 'w') as output_file:
-            output_file.write(out)
-
+def fail(module, message, **kwargs):
+    module.fail_json(msg=message, failed=True, goss_failed=True, **kwargs)
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
             path=dict(required=True, type='str'),
-            format=dict(required=False, type='str'),
-            output_file=dict(required=False, type='str'),
+            format=dict(required=False, type='str', choices=GOSS_OUTPUT_FORMATS),
             executable=dict(required=False, type='str', default='goss')
         ),
         supports_check_mode=False
     )
 
-    test_file_path = module.params['path']  # test file path
-    output_format = module.params['format']  # goss output format
-    output_file_path = module.params['output_file']
+    test_file = os.path.expanduser(module.params['path'])  # test file path
+    fmt = module.params['format']  # goss output format
     executable = module.params['executable']
 
-    if test_file_path is None:
-        module.fail_json(msg="test file path is null")
+    if not test_file or len(test_file) == 0:
+        fail(module, "Goss test file is undefined.")
 
-    test_file_path = os.path.expanduser(test_file_path)
+    # test if test file is a directory
+    if os.path.isdir(test_file):
+        fail(module, "Goss test file {} cannot be a directory.".format(test_file))
 
-    # test if access to test file is ok
+    # test if the test file is readable
+    if not os.access(test_file, os.R_OK):
+        fail(module, "Goss test file {} is not readable.".format(test_file))
 
-    if not os.access(test_file_path, os.R_OK):
-        module.fail_json(msg="Test file %s not readable" % (test_file_path))
+    rc, stdout, stderr = evaluate(module, test_file, fmt, executable)
 
-    # test if test file is not a dir
-    if os.path.isdir(test_file_path):
-        module.fail_json(msg="Test file must be a file ! : %s" % (test_file_path))
+    result = dict(rc=rc, stdout=stdout, stderr=stderr)
 
-    (rc, out, err) = check(module, test_file_path, output_format, executable)
+    succeed(module, **result) if rc == 0 else fail(module, "Goss Tests Failed.", **result)
 
-    if output_file_path is not None:
-        output_file_path = os.path.expanduser(output_file_path)
-        # check if output_file is a file
-        if output_file_path.endswith(os.sep):
-            module.fail_json(msg="output_file must be a file. Actually :  %s "
-                             % (output_file_path))
 
-        output_dirname = os.path.dirname(output_file_path)
-
-        # check if output directory exists
-        if not os.path.exists(output_dirname):
-            module.fail_json(msg="directory %s does not exists" % (output_dirname))
-
-        # check if writable
-        if not os.access(os.path.dirname(output_file_path), os.W_OK):
-            module.fail_json(msg="Destination %s not writable" % (os.path.dirname(output_file_path)))
-        # write goss result on the output file
-        output_file(output_file_path, out)
-
-    if rc is not None and rc != 0:
-        error_msg = "Goss Tests Failed"
-        module.fail_json(msg=error_msg, stderr=err, stdout=out)
-
-    result = {}
-    result['stdout'] = out
-    result['changed'] = False
-
-    module.exit_json(**result)
-
-main()
+if __name__ == "__main__":
+    main()
